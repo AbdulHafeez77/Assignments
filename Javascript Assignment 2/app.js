d2348813e9db7a4659b2be8b73850c490c01f033
solangihafees737@gmail.com <120651099+hafeez3568834@users.noreply.github.com>
2023-02-04 00:49:51 +0500

    Assignment 2 submit

--- Javascript Assignment 2/app.js
+++ Javascript Assignment 2/app.js
@@ -30,15 +30,16 @@
 // var pizza = "pizza \n pizz \n piz \n piz \n pi \n p";
 // alert (pizza);
 
+
 // Question 6
 // var myEmail = "My email address issolangihafeez737@gmail.com";
-// aler'(email);
+// aler'(myEmail);
 
 
 // Question 7
 // var book = "A smarter way to learn JavaScript.";
 // book = "Iam trying to learn JavaScript. A smarter way to learn JavaScript";
-// alert ("book");
+// alert (book);
 
 
 // Question 8
@@ -46,15 +47,71 @@
 
 
 //Question 9
-// alert ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+// var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert (design);
 
 
 
 // Chapter 3
+
 // Question no 1
-var age = "I am 15 years old";
-alert ("age");
+// var age = "I am 15 years old";
+// alert (age);
 
 // Question no 2 
-// var abc = "you have visited this webside 14 times";
-// alert ("abc")
+// var visited = "you have visited this webside 14 times";
+//  alert (visited)
+
+// Question no 3
+// var birthYear = ' My birth year is 2005';
+// document.write (birthYear);
+
+// Question no 4
+// var visiter = " John Doe ";
+// var product = " T-shirts ";
+// var quantity = " ordered 5 T-Shirt(s) on XYZ Clothing Store ";
+// document.write (visiter, product, quantity)
+
+
+
+// Chapter 4
+
+// Question no 1
+// var variable1 = "variable1";
+// var variable2 = "variable2";
+// var variable3 = "variable3";
+
+// Question no 2
+// 5 Illegal variable names:
+// var vari able = "";
+// var vari,able = "";
+// var var = "";
+// var vari/able = "";
+// var varia-ble = "";
+
+// 5 legal variable names:
+// var variable = "";
+// var vari_able = "";
+// var Variable = "";
+// var var1 = "";
+// var vari$able = "";
+
+
+// Question no 3
+// a)
+// document.write("<h1> Rules for naming JS variables </h1>")
+
+// b)
+// Variable names can only contain:
+// numbers , dollar signs , underscores, and camelCase.
+
+// c)
+// Variables must begin with:
+// dollar sign , underscores, or  small-letter, 
+ 
+// d)
+// camelCase
+
+// e)
+// Keywords
+
