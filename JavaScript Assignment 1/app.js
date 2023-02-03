4511ad2ebc353b4aa97d9e6abd915bdbf11efbad
solangihafees737@gmail.com <120651099+hafeez3568834@users.noreply.github.com>
2023-02-03 22:42:47 +0500

    added

---
+++ JavaScript Assignment 1/app.js
@@ -0,0 +1,18 @@
+// Chapter 1
+
+// Question no 1
+// alert("Welcome to our website.")
+
+
+// Question no 2 
+// alert("Error! Please enter a valid password.")
+
+// Question no 3
+//  alert("Welcome to JS land...\n Happy Coding!")
+
+// Question no 4
+// alert("Welcome to JS land...")
+// // alert("Happy Coding! \n Prevent this page from creating additional dailogs.")
+
+// Question no 5
+// alert("Hello... I can run JS through my web browser's console.")
---
+++ Javascript Assignment 2/app.js
@@ -0,0 +1,60 @@
+// Chapter 2
+
+// Question 1
+// var username = ;
+
+// Question 2
+// var myName = "Abdul Hafeez";
+
+
+// Question 3
+// a:
+// var message ;
+// b:
+// message = "Hello World";
+// c:
+// alert (message);
+
+
+// Question 4
+// var bioData = "jhone doe";
+// alert (bioData);
+
+// var bioData = "15 years old";
+// alert (bioData);
+
+// var bioData = "Certified Mobile Application Development";
+// alert (bioData);
+
+// Question 5
+// var pizza = "pizza \n pizz \n piz \n piz \n pi \n p";
+// alert (pizza);
+
+// Question 6
+// var myEmail = "My email address issolangihafeez737@gmail.com";
+// aler'(email);
+
+
+// Question 7
+// var book = "A smarter way to learn JavaScript.";
+// book = "Iam trying to learn JavaScript. A smarter way to learn JavaScript";
+// alert ("book");
+
+
+// Question 8
+// document.write ("Yah! i can write HTML content through JavaScript");
+
+
+//Question 9
+// alert ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+
+
+// Chapter 3
+// Question no 1
+var age = "I am 15 years old";
+alert ("age");
+
+// Question no 2 
+// var abc = "you have visited this webside 14 times";
+// alert ("abc")
