7693f06bd27e01b752a30d7b2e584e39546c2a95
solangihafees737@gmail.com <120651099+hafeez3568834@users.noreply.github.com>
2023-02-05 01:38:06 +0500

    Complete Chapter No 5

---
+++ JavaScript Assignment 3/app.js
@@ -0,0 +1,70 @@
+// Chapter 5
+
+// Question no 1
+// var num = 5;
+// var num2 = 3;
+// document.write (num + num2);
+
+// Question no 2
+// var num = 5;
+// var num2 = 3;
+
+// document.write (num - num2);
+
+// document.write (num * num2);
+
+// document.write (num / num2);
+
+// Question no 3
+// a)
+// var variable =
+
+// b)
+// document.write ( "Value after variable declaration is: <br>");
+
+// c)
+// variable = 5;
+
+// d)
+// document.write ("Initial value: ");
+// document.write (variable);
+
+// e)
+// variable = 5;
+
+// f)
+// document.write("<br> Value after increment is: ");
+// document.write(++variable);
+
+// g)
+// variable = variable + 7;
+
+// h)
+// document.write ("<br> Value after addition is: ");
+// document.write (variable);
+
+// i)
+// variable = --variable;
+
+// j)
+// document.write("<br> Value after decrement is: ");
+// document.write (variable);
+
+// k)
+// variable = variable / 3;
+
+// l)
+// document.write("<br> The Remiander is: ")
+// document.write (0)
+
+
+
+
+
+
+
+
+
+
+
+
