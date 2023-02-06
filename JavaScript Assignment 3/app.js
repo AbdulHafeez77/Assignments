ec9fac22b3a50a231a757a2674f68b20bb0750f8
solangihafees737@gmail.com <120651099+hafeez3568834@users.noreply.github.com>
2023-02-06 16:36:51 +0500

    completed chapter 6-9

--- JavaScript Assignment 3/app.js
+++ JavaScript Assignment 3/app.js
@@ -1,71 +1,200 @@
-// Chapter 5
+// // Chapter 5
 
-// Question no 1
-// var num = 5;
-// var num2 = 3;
-// document.write (num + num2);
+// // Question no 1
+// // var num = 5;
+// // var num2 = 3;
+// // document.write (num + num2);
 
-// Question no 2
-// var num = 5;
-// var num2 = 3;
+// // Question no 2
+// // var num = 5;
+// // var num2 = 3;
 
-// document.write (num - num2);
+// // document.write (num - num2);
 
-// document.write (num * num2);
+// // document.write (num * num2);
 
-// document.write (num / num2);
+// // document.write (num / num2);
 
-// Question no 3
-// a)
-// var variable =
+// // Question no 3
+// // a)
+// // var variable =
 
-// b)
-// document.write ( "Value after variable declaration is: <br>");
+// // b)
+// // document.write ( "Value after variable declaration is: <br>");
 
-// c)
-// variable = 5;
+// // c)
+// // variable = 5;
 
-// d)
-// document.write ("Initial value: ");
-// document.write (variable);
+// // d)
+// // document.write ("Initial value: ");
+// // document.write (variable);
 
-// e)
-// variable = 5;
+// // e)
+// // variable = 5;
 
-// f)
-// document.write("<br> Value after increment is: ");
-// document.write(++variable);
+// // f)
+// // document.write("<br> Value after increment is: ");
+// // document.write(++variable);
 
-// g)
-// variable = variable + 7;
+// // g)
+// // variable = variable + 7;
 
-// h)
-// document.write ("<br> Value after addition is: ");
-// document.write (variable);
+// // h)
+// // document.write ("<br> Value after addition is: ");
+// // document.write (variable);
 
-// i)
-// variable = --variable;
+// // i)
+// // variable = --variable;
 
-// j)
-// document.write("<br> Value after decrement is: ");
-// document.write (variable);
+// // j)
+// // document.write("<br> Value after decrement is: ");
+// // document.write (variable);
 
-// k)
-// variable = variable / 3;
+// // k)
+// // variable = variable / 3;
 
-// l)
-// document.write("<br> The Remiander is: ");
-// document.write (0);
-
-
-// Question no 4
-// var ticket = 600;
-// ticket = 600 * 5;
-// document.write ("Total coast to buy 5 ticket to a movie is ");
-// document.write (ticket, " PKR ");
+// // l)
+// // document.write("<br> The Remiander is: ");
+// // document.write (0);
 
 
-// Question no 5
+// // Question no 4
+// // var ticket = 600;
+// // ticket = 600 * 5;
+// // document.write ("Total coast to buy 5 ticket to a movie is ");
+// // document.write (ticket, " PKR ");
+
+
+// // Question no 5
+// // document.write("Table of 2");
+// // var num1 = 2 * 1;
+// // document.write (' <br> 2 * 1 = ', num1);
+// // var num2 = 2 * 2;
+// // document.write (' <br> 2 * 2 = ', num2);
+// // var num3 = 2 * 3;
+// // document.write (' <br> 2 * 3 = ', num3);
+// // var num4 = 2 * 4;
+// // document.write (' <br> 2 * 4 = ',num4);
+// // var num5 = 2 * 5;
+// // document.write (' <br> 2 * 5 = ', num5);
+// // var num6 = 2 * 6;
+// // document.write (' <br> 2 * 6 = ', num6);
+// // var num7 = 2 * 7;
+// // document.write (' <br> 2 * 7 = ', num7);
+// // var num8 = 2 * 8;
+// // document.write (' <br> 2 * 8 = ', num8);
+// // var num9 = 2 * 9;
+// // document.write (' <br> 2 /* 9 = ', num9);
+// // var num10 = 2 * 10;
+// // document.write (' <br> 2 * 10 = ', num10);
+
+// // Question no 6
+
+// // var c = 22;
+// // var c_into_f = (22 * 9 /5) + 32; 
+// // document.write  (c + "°C is " + c_into_f + "°F");
+// // var f = 75;
+// // var f_into_c = (75-32) * 5 / 9;
+// // document.write ("<br>" + f + "°F is " + f_into_c + "°C") 
+
+// // Question no 7
+
+// // var poi = 650;
+// // var qoi = 3;
+// // var poi2 = 100;
+// // var qoi2 = 7;
+// // var total = 2750;
+// // document.write ("<br> Price of item 1 is " + poi);
+// // document.write ("<br> Quantity of item 1 is "+ qoi);
+// // document.write ("<br> Price of item 2 is "+ poi2);
+// // document.write ("<br> Quantity of item 1 is "+ qoi2);
+// // document.write ("<br> Shipping Charges "+ poi2);
+// // document.write ("<br> Total cost of your order "+ total);
+
+// // Question no 8
+// // var total = 980;
+// // var obtain = 804;
+// // var result = obtain / total * 100;
+// // document.write("<br> Total marks: " + total);
+// // document.write("<br> Marks obtained: " + obtain);
+// // document.write("<br> Percentage " + result);
+
+// // Question no 9
+// // var dollar = 104.80;
+// // var riyal = 28;
+// // var total = dollar * 10 + riyal * 25;
+// // document.write("Total Currency in PKR: "+ total);
+
+// // Question no 10
+// // var num = 4;
+// // var num2 = num + 5 * 10 / (5) ;
+// // alert(num2);
+
+// // Question no 11
+// // var current = 2023;
+// // var birth = 2003;
+// // var age = 19;
+// // document.write("<br> Current Year: " + current);
+// // document.write("<br> Birth Year: " + birth);
+// // document.write("<br> Your Age: " + age);
+
+// // Question no 12
+// // var radius = 30;
+// // var circum = (radius * 2 )* 3.14;
+// // var area = 3.14 * 900;
+// // document.write("<br> Radius of a Circle: " + radius)
+// // document.write("<br> The Circumference is: " + circum)
+// // document.write("<br> The area is: " + area)
+
+// // Question no 13
+// // var fvt_s = "Samosa"
+// // var c_age = 15;
+// // var max_age = 65;
+// // var amount_d = 3; 
+// // document.write("<br> Favorite snack: " + fvt_s);
+// // document.write("<br> Current age: " + c_age);
+// // document.write("<br> Estimated maximum age: " + max_age);
+// // document.write("<br> Amount of snacks per day: " + amount_d);
+// // document.write("<br> You will need 150 Samosan to last you untill the ripe old age of 65")
+
+
+// // Chapter no 6-9
+
+// // Question no 1
+
+// // var num = 5;
+// // var num2 = ++num;
+// // var num3 = ++num2 + 2;
+// // var num4 = --num3;
+// // var num5 = --num4 - 2;
+// // document.write("<h1>Result</h1>")
+// // document.write("<br> The value of number is: " + num)
+// // document.write("<hr>")
+// // document.write("<br> The value of ++number is: " + num2)
+// // document.write("<br> Now the value of ++number is: " + num2)
+// // document.write("<br> The value of ++number is: " + num3)
+// // document.write("<br> Now the value of ++number is: " + num3)
+// // document.write("<br> The value of --number is: " + num4)
+// // document.write("<br> Now the value of --number is: " + num4)
+// // document.write("<br> The value of --number is: " + num5)
+// // document.write("<br> Now the value of --number is: " + num5)
+
+
+// // Question no 2
+// // var a = 2;
+// // var b = 1;
+// // var result = --a - --b + ++b + b-- ;
+// // document.write("a is: " + a)
+// // document.write("b is: " + b)
+// // document.write("Result is: " + result)
+// // Result = 3
+
+// // Question no 3
+// // prompt("Welcome! \n Enter your Name.")
+
+// // Question no 4
+// prompt("Enter a number and watch it's table")
+// if (prompt===2) {
 // document.write("Table of 2");
 // var num1 = 2 * 1;
 // document.write (' <br> 2 * 1 = ', num1);
@@ -87,7 +216,204 @@
 // document.write (' <br> 2 /* 9 = ', num9);
 // var num10 = 2 * 10;
 // document.write (' <br> 2 * 10 = ', num10);
+// }
+// if (prompt===3) {
+// document.write("Table of 3");
+// var num1 = 3 * 1;
+// document.write (' <br> 3 * 1 = ', num1);
+// var num2 = 3 * 2;
+// document.write (' <br> 3 * 2 = ', num2);
+// var num3 = 3 * 3;
+// document.write (' <br> 3 * 3 = ', num3);
+// var num4 = 3 * 4;
+// document.write (' <br> 3 * 4 = ',num4);
+// var num5 = 3 * 5;
+// document.write (' <br> 3 * 5 = ', num5);
+// var num6 = 3 * 6;
+// document.write (' <br> 3 * 6 = ', num6);
+// var num7 = 3 * 7;
+// document.write (' <br> 3 * 7 = ', num7);
+// var num8 = 3 * 8;
+// document.write (' <br> 3 * 8 = ', num8);
+// var num9 = 3 * 9;
+// document.write (' <br> 3 /* 9 = ', num9);
+// var num10 = 3 * 10;
+// document.write (' <br> 3 * 10 = ', num10);
+// }
+// if (prompt===4) {
+// document.write("Table of 4");
+// var num1 = 4 * 1;
+// document.write (' <br> 4 * 1 = ', num1);
+// var num2 = 4 * 2;
+// document.write (' <br> 4 * 2 = ', num2);
+// var num3 = 4 * 3;
+// document.write (' <br> 4 * 3 = ', num3);
+// var num4 = 4 * 4;
+// document.write (' <br> 4 * 4 = ',num4);
+// var num5 = 4 * 5;
+// document.write (' <br> 4 * 5 = ', num5);
+// var num6 = 4 * 6;
+// document.write (' <br> 4 * 6 = ', num6);
+// var num7 = 4 * 7;
+// document.write (' <br> 4 * 7 = ', num7);
+// var num8 = 4 * 8;
+// document.write (' <br> 4 * 8 = ', num8);
+// var num9 = 4 * 9;
+// document.write (' <br> 4 /* 9 = ', num9);
+// var num10 = 4 * 10;
+// document.write (' <br> 4 * 10 = ', num10);
+// }
+// if (pro===6) {
+// document.write("Table of 6");
+// var num1 = 6 * 1;
+// document.write (' <br> 6 * 1 = ', num1);
+// var num2 = 6 * 2;
+// document.write (' <br> 6 * 2 = ', num2);
+// var num3 = 6 * 3;
+// document.write (' <br> 6 * 3 = ', num3);
+// var num4 = 6 * 4;
+// document.write (' <br> 6 * 4 = ',num4);
+// var num5 = 6 * 5;
+// document.write (' <br> 6 * 5 = ', num5);
+// var num6 = 6 * 6;
+// document.write (' <br> 6 * 6 = ', num6);
+// var num7 = 6 * 7;
+// document.write (' <br> 6 * 7 = ', num7);
+// var num8 = 6 * 8;
+// document.write (' <br> 6 * 8 = ', num8);
+// var num9 = 6 * 9;
+// document.write (' <br> 6 /* 9 = ', num9);
+// var num10 = 6 * 10;
+// document.write (' <br> 6 * 10 = ', num10);
+// }
+// if (pro===7) {
+// document.write("Table of 7");
+// var num1 = 7 * 1;
+// document.write (' <br> 7 * 1 = ', num1);
+// var num2 = 7 * 2;
+// document.write (' <br> 7 * 2 = ', num2);
+// var num3 = 7 * 3;
+// document.write (' <br> 7 * 3 = ', num3);
+// var num4 = 7 * 4;
+// document.write (' <br> 7 * 4 = ',num4);
+// var num5 = 7 * 5;
+// document.write (' <br> 7 * 5 = ', num5);
+// var num6 = 7 * 6;
+// document.write (' <br> 7 * 6 = ', num6);
+// var num7 = 7 * 7;
+// document.write (' <br> 7 * 7 = ', num7);
+// var num8 = 7 * 8;
+// document.write (' <br> 7 * 8 = ', num8);
+// var num9 = 7 * 9;
+// document.write (' <br> 7 /* 9 = ', num9);
+// var num10 = 7 * 10;
+// document.write (' <br> 7 * 10 = ', num10);
+// }
+// if (pro===8) {
+// document.write("Table of 2");
+// var num1 = 8 * 1;
+// document.write (' <br> 8 * 1 = ', num1);
+// var num2 = 8 * 2;
+// document.write (' <br> 8 * 2 = ', num2);
+// var num3 = 8 * 3;
+// document.write (' <br> 8 * 3 = ', num3);
+// var num4 = 8 * 4;
+// document.write (' <br> 8 * 4 = ',num4);
+// var num5 = 8 * 5;
+// document.write (' <br> 8 * 5 = ', num5);
+// var num6 = 8 * 6;
+// document.write (' <br> 8 * 6 = ', num6);
+// var num7 = 8 * 7;
+// document.write (' <br> 8 * 7 = ', num7);
+// var num8 = 8 * 8;
+// document.write (' <br> 8 * 8 = ', num8);
+// var num9 = 8 * 9;
+// document.write (' <br> 8 /* 9 = ', num9);
+// var num10 = 8 * 10;
+// document.write (' <br> 8 * 10 = ', num10);
+// }
+// if (pro===9) {
+// document.write("Table of 2");
+// var num1 = 9 * 1;
+// document.write (' <br> 9 * 1 = ', num1);
+// var num2 = 9 * 2;
+// document.write (' <br> 9 * 2 = ', num2);
+// var num3 = 9 * 3;
+// document.write (' <br> 9 * 3 = ', num3);
+// var num4 = 9 * 4;
+// document.write (' <br> 9 * 4 = ',num4);
+// var num5 = 9 * 5;
+// document.write (' <br> 9 * 5 = ', num5);
+// var num6 = 9 * 6;
+// document.write (' <br> 9 * 6 = ', num6);
+// var num7 = 9 * 7;
+// document.write (' <br> 9 * 7 = ', num7);
+// var num8 = 9 * 8;
+// document.write (' <br> 9 * 8 = ', num8);
+// var num9 = 9 * 9;
+// document.write (' <br> 9 /* 9 = ', num9);
+// var num10 = 9 * 10;
+// document.write (' <br> 9 * 10 = ', num10);
+// }
+// else{
+// document.write("Table of 5");
+// var num1 = 5 * 1;
+// document.write (' <br> 5 * 1 = ', num1);
+// var num2 = 5 * 2;
+// document.write (' <br> 5 * 2 = ', num2);
+// var num3 = 5 * 3;
+// document.write (' <br> 5 * 3 = ', num3);
+// var num4 = 5 * 4;
+// document.write (' <br> 5 * 4 = ',num4);
+// var num5 = 5 * 5;
+// document.write (' <br> 5 * 5 = ', num5);
+// var num6 = 5 * 6;
+// document.write (' <br> 5 * 6 = ', num6);
+// var num7 = 5 * 7;
+// document.write (' <br> 5 * 7 = ', num7);
+// var num8 = 5 * 8;
+// document.write (' <br> 5 * 8 = ', num8);
+// var num9 = 5 * 9;
+// document.write (' <br> 5 /* 9 = ', num9);
+// var num10 = 5 * 10;
+// document.write (' <br> 5 * 10 = ', num10);
+// }
 
+
+// Question no 6
+// document.write("Subject"+" ");
+// document.write("Total Marks"+ " ");
+// document.write("Obtained Maks" + " ");
+// document.write("Presentage" + " " + "<br>");
+// var sub1 = prompt("Enter Your Subject 1");
+// document.write ( sub1 + "   "+ "   " + "  ");
+// var marks1 = +prompt("Enter Total Marks of Your Subject 1");
+// document.write( marks1 + "   "+ "  "+ "  ");
+// var obtain1 = +prompt("Enter obtain marks of your Subject 1");
+// document.write(obtain1+ "   "+ "  "+ "  ");
+// var Percentage1 = obtain1 / marks1 * 100;
+// document.write(Percentage1 + "%" + "<br>") ;
+// var sub2 = prompt("Enter Your Subject 2");
+// document.write (sub2) + "  " + "  "+ "  ";
+// var marks2 = +prompt("Enter Total Marks of Your Subject 2");
+// document.write(marks1 + "  "+ "  "+ "  ");
+// var obtain2 = +prompt("Enter obtain marks of your Subject 2");
+// document.write(obtain2 + "  "+ "  "+ "  ");
+// var Percentage1 = obtain1 / marks1 * 100;
+// document.write(Percentage1 + "%" + "<br>") ;
+// var sub3 = prompt("Enter Your Subject 3");
+// document.write (sub3 + "  "+ "  "+ "  ");
+// var marks3 = +prompt("Enter Total Marks of Your Subject 3");
+// document.write(marks1 + "  "+ "  "+ "  ");
+// var obtain3 = +prompt("Enter obtain marks of your Subject 3");
+// document.write(obtain3 + "  "+ "  "+ "  ");
+// var Percentage1 = obtain1 / marks1 * 100;
+// document.write(Percentage1 + "%" +"<br>") ;
+// document.write(marks1 + marks2 + marks3 + " ");
+// document.write(obtain1 + obtain2 + obtain3 + " ");
+// var p = Percentage1 + Percentage1 + Percentage1;
+// var result = p / 1000 * 100;
+// document.write(result + "%")
 
 
 
