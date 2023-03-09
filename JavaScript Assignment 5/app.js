161c2d485cc161aa2c3216b0067fe41c063594e0
AbdulHafeez77 <solangihafeez737@gmail.com>
2023-03-09 18:35:30 +0500

    submitting assignment 6

--- JavaScript Assignment 5/app.js
+++ JavaScript Assignment 5/app.js
@@ -51,6 +51,7 @@
 // document.write(colors + "<br>");
 
 // var input = prompt("In which color do you want to add in the begining.");
+// document.write("Your list: <br>");
 
 // if (input == colors[0]) {
 //     colors.splice(0,1);
@@ -82,7 +83,9 @@
 //     colors.unshift(input);
 //     document.write(colors);
 // }
-
+// else{
+//     document.write("Please enter the color which is given in the list.")
+// }
 
 
 // // b)
@@ -118,6 +121,9 @@
 //     colors.push(input);
 //     document.write(colors);
 // }
+// else{
+//     document.write("Please enter the color which is given in the list.")
+// }
 
 
 // c)
@@ -141,44 +147,43 @@
 // document.write(colors);
 
 
-// g)
+// // g)
 // var input4 = prompt("Enter a color which you want to delete from the list.");
 
 // if (input4 == colors[0]) {
-    // colors.shift();
-    // document.write(colors);
+//     colors.shift();
+//     document.write(colors);
 // }
 // else if (input4 == colors[1]) {
-    // colors.splice(1,1);
-    // document.write(colors);
+//     colors.splice(1,1);
+//     document.write(colors);
 // }
 // else if (input4 == colors[2]) {
-    // colors.splice(2,1);
-    // document.write(colors);
+//     colors.splice(2,1);
+//     document.write(colors);
 // }
 // else if (input4 == colors[3]) {
-    // colors.splice(3,1);
-    // document.write(colors);
+//     colors.splice(3,1);
+//     document.write(colors);
 // }
 // else if (input4 == colors[4]) {
-    // colors.splice(4,1);
-    // document.write(colors);
+//     colors.splice(4,1);
+//     document.write(colors);
 // }
 // else if (input4 == colors[5]) {
-    // colors.splice(5,1);
-    // document.write(colors);
+//     colors.splice(5,1 "");
+//     document.write(colors);
 // }
-
-    // colors.splice(0,2);
-    // document.write(input4 , colors);
-
-
+// else{
+//     document.write("Please enter the color which is given in the list.");
+// }
 
 
 // Question no 10
 // var scores = [320, 230, 480, 120];
 // document.write("Scores of students: " + scores + "<br>");
 // document.write("Ordered scores of students: " + [scores[3], scores[1] ,  scores[0],  scores[2]] + "<br>");
+
 
 
 // Qustion no 11
@@ -189,10 +194,12 @@
 // document.write("Selected Cites: " + "<br>" + del);
 
 
+
 // Question no 12
 // var arr = ["This" , "is" , "my",  "cat"];
 // document.write("Arrays" + "<br>" + arr);
 // document.write("String" + "<br>" + arr[0]+ " " + arr[1] + " " + arr[2] + " " + arr[3]);
+
 
 
 // Question no 13
@@ -252,10 +259,126 @@
 // Chapter no 17-20
 
 // Question no 1
+// var arr = [[]];
+
+
+// Question no 2 
+// arr = [[0,1,2,3,],[1,0,1,2],[2,1,0,1]];
+// document.write(arr[0] + "<br>" + arr[1] + "<br>" + arr[2]);
+
+
+// Question no 3
+// for(var i = 0; i <= 10; i++){
+    // document.write(i + "<br>");
+// }
+
+
+// Question no 4
+// var input = prompt("Enter a number to show it's multiplication table.");
+// var input2 = prompt("Enter length multiplication table.");
+
+// for(var i = 1; i <= input2; i++){
+    // document.write(input + "x" + i + "=" + input * i + "<br>") ;
+// }
+
+
+// Question no 5
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// for (var i = 0; i < fruits.length; i++){
+//     document.write(fruits[i] + "<br>");
+//     document.write("Element at index " + i + " is " + fruits[i] + "<br>"); 
+// }
+
+
+// // Question no 6
+
+// a)
+// document.write("Counting." + "<br>")
+// for (var i = 1; i <= 15; i++){
+//     document.write(i + " ");
+// }
+
+// b)
+// document.write("Reverse Counting." + "<br>")
+// for (var i = 10; i >= 1; i--){
+//     document.write(i + " ");
+// }
+
+// c)
+// document.write("Even." + "<br>")
+// for (var i = 1; i <= 10; i++){
+    // document.write(i*2 + " ");
+// }
+
+// d) 
+// document.write("<br>" + "Odd." + "<br>")
+// for (var i = 1; i<= 20; i += 2){
+// document.write(i + " ");
+// }
+
+// e)
+// document.write("Series." + "<br>")
+// for (var i = 1; i <= 10; i++){
+//     document.write(i*2 + "k" + " ");
+// }
+
+
+// Question no 7
+var a = ["cake", "apple pie", "cookie", "chips", "patties"];
+var input = prompt("Welcome to my Bakery. \n What do you want to order?");
+var flag = false;
+
+for(var i = 0; i < a.length; i++){
+    if (a[i] == input) {
+    flag = true; }
+}
+if(flag){
+    document.write(input + " is available at index " +  + " in our bakery.");
+}
+else{
+    document.write("We are sorry." + input + " is not available in our bakery.");
+}
 
 
 
+// Question no 8
+// var arr = [24, 53, 78, 91, 12];
+// var a = arr[0];
 
+// document.write("Arrays items: " + arr + "<br>");
+
+// for(var i = 0; i < arr.length; i++){
+    // if(arr[i] > a){
+        // a = arr[i];
+    // }
+// }
+
+// document.write(a + " is the largest number.");
+
+
+
+// Question no 9
+
+// var arr = [24, 53, 78, 91, 12];
+// var a = arr[0];
+
+// document.write("Arrays items: " + arr + "<br>");
+
+// for(var i = 0; i < arr.length; i++){
+//     if(arr[i] < a){
+//         a = arr[i];
+//     }
+// }
+
+// document.write(a + " is the largest number.");
+
+
+
+// Question no 10
+// for(var i = 1; i <= 20; i++){
+// document.write(5 * i + " , ") ;
+// }
 
 
 
---
+++ JavaScript Assignment 6/app.js
@@ -0,0 +1,120 @@
+
+// Chapter No 21 to 25
+  
+// Question No 1
+// var firstName = prompt("Enter your  First name");
+// var secName = prompt("Enter your Second name");
+// var fullname = firstName + " " + secName;
+
+// alert("Good Morning " +  fullname + ".");
+
+
+// Question no 2
+// var mobile = prompt("What is your favorite mobile phone model ?");
+// var a = "My favorite phone is " + mobile + "<br>" + "length of string " + mobile.length;
+
+// document.write(a)
+
+
+
+// Question no 3
+// var string = "Pakistan";
+// var n = string.indexOf("n");
+// document.write("String: " + string + "<br>" + "Index of 'n': " + n);
+
+
+
+// Question no 4
+// var str = "Hello world";
+// var l = str.lastIndexOf("l");
+// document.write("String: " + str + "<br>" + "last Index of 'l': " + l);
+
+
+// Question no 5
+// var str = "Pakistan";
+// document.write("String: " + str + "<br>" + " Character at Index '3': " + str[3]);
+
+
+
+// Quwstion no 6
+
+
+// Question no 7
+
+// replacing string value 
+// var str = "Hyderabad";
+// var index = str.slice(0,4);
+
+// if(index){
+//    var  first= "Islam";
+//    var sec = str.slice(5);
+// }
+// console.log("City: " + str + " " + "<br>" + " After replacement: " + first + sec);
+
+
+
+// Question no 8
+// var input = prompt("Enter your message.")
+// input = input.replace("and" , "&");
+
+// console.log(input);
+
+
+// Question no 9
+// var input = "472";
+// +input;
+
+// document.write("Value: " + input + "<br>" +
+//                "Type : " + "string" + "<br>" +
+//                "Value: " + input + "<br>" + 
+//                "Type : " + "number" + "<br>");
+
+
+
+
+// Question no 10
+// var input = prompt("Enter anything...")
+
+// document.write("User input: " + input + "<br>" + "Upper case: " + input.toUpperCase());
+
+
+
+// Question no 11
+// var input = prompt("Enter a single word")
+
+// var firstLetter = input[0].toUpperCase();
+// var result = firstLetter + input.slice(1);
+
+// console.log(result);
+
+
+
+// // Question no 12
+// var num = 35.36;
+// var num2 = num.toString().split("");
+// num2.splice(2,1);
+
+// console.log(num2.join(""));
+
+
+
+// Question no 13
+// var input =  prompt("Please enter your username.");
+// var userInput = input;
+
+// for(var i = 0; i < userInput.length; i++){
+//     if(userInput[i] == "@" || userInput[i] == "." || userInput[i] == ","|| userInput[i] == "!"){
+//         alert("Please enter a valid username");
+//     }
+// }
+
+
+
+// Question no 14
+
+
+
+
+
+
+
