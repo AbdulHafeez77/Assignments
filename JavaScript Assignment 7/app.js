c49cba1dc6c48fa6ec9bba8caaf62894833c6534
AbdulHafeez77 <solangihafeez737@gmail.com>
2023-04-07 22:46:29 +0500

    submiting assignment 7

---
+++ JavaScript Assignment 7/app.js
@@ -0,0 +1,72 @@
+// Chapter 26 - 30
+
+// Question no 1
+
+// var number = prompt(Enter a positive integer number."");
+// var num = Number(number);
+// var round = Math.round(number);
+// var floor = Math.floor(number);
+// var ceil = Math.ceil(number);
+
+// console.log(num);
+// console.log(round);
+// console.log(floor);
+// console.log(ceil);
+
+
+
+// Question no 2
+
+// var number = prompt("Enter a positive integer number.");
+// var num = Number(number);
+// var round = Math.round(number);
+// var floor = Math.floor(number);
+// var ceil = Math.ceil(number);
+
+// console.log(num);
+// console.log(round);
+// console.log(floor);
+// console.log(ceil);
+
+
+
+// Question no 3
+
+// var number = +prompt("Enter a number: ");
+
+// if (number < 0){
+//     var absolute_value = -number
+// }
+// else{
+//     var absolute_value = number;
+// }
+
+// document.write("The absolute value of ", number, " is ", absolute_value); 
+
+
+
+// Question no 4
+// var random = Math.round(Math.random() * 6)
+// console.log("The value of dice: "+ random);
+
+
+
+
+// Question no 6
+// var randomNum = Math.round(Math.random() * 100)
+// console.log("Random Number between 1 and 100: "+ randomNum);
+
+
+
+// Question no 8
+// var inputNum = prompt("Enter a number between 1 and 100.");
+// var randomNum = Math.round(Math.random() * 100);
+
+// if(inputNum == randomNum){
+//     alert("your number is matched with random number");
+// }else{
+//     alert("Try Again. \n your number is not matched wih random number;")
+// }
+
+
+
